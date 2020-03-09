7e1511b41b15255390b5aa5083d5484f5605ae70
huzor <huzor@terpmail.umd.edu>
2020-03-09 11:13:34 -0400

    Sixth Commit

--- url_scraper.py
+++ url_scraper.py
@@ -2,7 +2,6 @@
 from bs4 import BeautifulSoup
 import requests
 import datetime
-#import html_scraper
 
 ### SEND REQUEST TO GET WEBPAGE WE WANT TO PARSE ###
 def getPage(url,headers):
@@ -15,7 +14,6 @@ url = 'https://www.bestbuy.com/site/desktop-computers/all-desktops/pcmcat1434000
 headers = {
     "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/77.0.3865.90 Chrome/77.0.3865.90 Safari/537.36"
     }
-
 web_page = getPage(url,headers)
 
 ### TURN WEBPAGE INTO OBJECT THAT CAN BE PARSED ###
@@ -45,29 +43,5 @@ with open('products.csv','w') as pr:
         item_link = product.a['href']
         find_price = product.find_all('span',{'aria-hidden':'true'})
         item_price = find_price[1].text
-        #print(item_price.replace(',',''))
 
         pr.write(item_id + ',' + item_name + ',' + item_rating + ',' + item_link + ',' + item_price.replace(',','') + '\n')
-
-
-    # print('item_name: ' + item_name)
-    # print('item_id: ' + item_id)
-    # print('item_rating ' + item_rating)
-    # print('item_link ' + item_link)
-    # print('item_price ' + item_price)
-
-
-
-### GET INFO ON OBJECT AND PLACE IN EMPTY .TXT FILE ###
-
-# tweet_urls = []
-# a_tags = parsed_page.find_all('a')
-# for element in a_tags:
-#     urls = element.get('href')
-#     tweet_urls.append(urls)
-#
-# with open('tweet_urls.txt','a') as tweet:
-#     now = datetime.datetime.now()
-#     tweet.write('%s\n'%now)
-#     for listitem in tweet_urls:
-#         tweet.write('%s\n' % listitem)
